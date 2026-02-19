44ad4b5df807c1a058debf572bbcc85866639c5f
samzanotti <sammy_zanotti@yahoo.com>
2026-02-19 10:11:28 -0700

    subscriber node

---
+++ clanker_interfaces/Camera_Sub_Node.py
@@ -0,0 +1,31 @@
+import rclpy
+from rclpy.node import Node
+import cv2
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+class ImageListener(Node):
+    def __init__(self):
+        super().__init__('image_listener')
+        self.sub = self.create_subscription(Image,'/camera/camera/color/image_raw',self.img_read, 10)   #sub to frames that are being transmitted, need to f
+        self.sub            #prevent unused variable warning
+        self.br = CvBridge()    #covert between ROS and OpenCV images
+
+    def img_read(self,msg: Image):
+        im_cap = cv2.VideoCapture(0,cv2.CAP_V4L2)
+
+    def listener_cb(self, data):
+        self.get_logger().info('Receiving video frame')
+        frame = self.br.imgmsg_to_cv2(data)
+        cv2.imshow("Intel RealSense Camera", frame) 
+
+        #cv2.waitKey(1)
+
+
+def main(args = None):
+    rclpy.init(args = args)
+    node = ImageListener()           #making the listener node
+    rclpy.spin(node)            #spin until it is told to stop
+    node.destroy_node()
+    rclpy.shutdown()
