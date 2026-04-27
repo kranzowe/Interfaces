bdcd771e4a9eca4857a02ee3bda1e9d4055f26ab
spyalexs <spyalexs@gmail.com>
2026-04-27 12:13:02 -0600

    bug8

--- clanker_hardware/src/lidar_bug.py
+++ clanker_hardware/src/lidar_bug.py
@@ -100,8 +100,8 @@ class WASDNode(Node):
 
         msg = Twist()
 
-        msg.linear.x = 1500
-        msg.angular.z = self.optimal_angle * 2 + 1500
+        msg.linear.x = 1500.0
+        msg.angular.z = self.optimal_angle * 2 + 1500.0
 
 
 
