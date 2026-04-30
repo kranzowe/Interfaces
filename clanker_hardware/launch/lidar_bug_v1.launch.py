8e59e147669fe5a0e2c97ad6ce2e13fcf563468b
kevtenly <kelvin.aladum@colorado.edu>
2026-04-30 15:07:25 -0600

    launch

---
+++ clanker_hardware/launch/lidar_bug_v1.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="clanker_hardware",
+            executable="lidar_bug_v1.py",
+            name="lidar_bug_v1",
+            parameters=[{
+                "neutral_steer": 1470.0,
+                "ol_speed": 1500.0,
+                "steer_p": 5.0,
+                "steer_d": 0.3,
+                "steer_d_filter": 0.2,
+                "steer_phi": 45.0,
+                "steer_max": 400.0,
+                "right_search_limit": 120.0,
+                "left_search_limit": 10.0,
+                "wall_ahead_distance": 1.5,
+                "range_threshold": 0.6,
+            }],
+        ),
+    ])
